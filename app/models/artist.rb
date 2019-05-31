309bba0cc1e13394bbdba55c0a1af466e0c3bd06
Ryan Pethel <arpethel@gmail.com>
2019-05-31 10:08:29 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,12 +5,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    binding.pry
     self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
